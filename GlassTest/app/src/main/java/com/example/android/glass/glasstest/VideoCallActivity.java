86aa0ede37106e3e4119b6a19cfc9d6ce2c7d9a0
Girish Karadas <girishkaradas@gmail.com>
2021-01-11 14:29:24 +0530

    Update 11/1

--- GlassTest/app/src/main/java/com/example/android/glass/glasstest/VideoCallActivity.java
+++ GlassTest/app/src/main/java/com/example/android/glass/glasstest/VideoCallActivity.java
@@ -1,18 +1,25 @@
 package com.example.android.glass.glasstest;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.glass.ui.GlassGestureDetector;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -22,9 +29,17 @@ import com.opentok.android.Publisher;
 import com.opentok.android.PublisherKit;
 import com.opentok.android.Subscriber;
 import com.opentok.android.OpentokError;
+import com.squareup.picasso.Picasso;
+
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.Manifest;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
@@ -37,6 +52,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -58,53 +74,104 @@ public class VideoCallActivity extends BaseActivity implements  Session.SessionL
     private Subscriber mSubscriber;
 
     private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference reference, ref1, ref2;
+    private DatabaseReference reference, ref1, ref_session, ref2;
     private StorageReference storageReference, storageRef1;
-    private ImageView imageView;
-
-
+    private RecyclerView rcView;
+    private ArrayList<DataUploads> arrayList =  new ArrayList<>();
+    private ImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_call);
 
-        imageView = findViewById(R.id.activity_video_call_imageview);
+        rcView = findViewById(R.id.activity_call_rcView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        rcView.setLayoutManager(layoutManager);
+        rcView.setHasFixedSize(true);
+        adapter = new ImageAdapter(this, arrayList);
+        rcView.setAdapter(adapter);
 
         requestPermissions();
         firebaseDatabase = FirebaseDatabase.getInstance();
         reference= firebaseDatabase.getReference();
         ref1= reference.child("calls");
-        ref2 = reference.child("session");
+        ref_session = reference.child("session");
 
         Date now = new Date();
         android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
         String key=ref1.push().getKey();
-        ref1.child(key).setValue(now);
-        ref2.setValue(key);
+        ref1.child(key).child("time").setValue(now.toString());
+        ref_session.setValue(key);
+        ref2 = reference.child("uploads").child(key);
         storageReference = FirebaseStorage.getInstance().getReference().child("images").child(key);
 
-        try {
-            File file = File.createTempFile("image", "jpg");
-            storageReference.getFile(file)
-                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-                            imageView.setImageBitmap(bitmap);
-                            imageView.setVisibility(View.VISIBLE);
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.e("CallActivity", "Error");
-                }
-            });
+        ref2.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                arrayList.add(new DataUploads(snapshot.getKey().toString(), snapshot.getValue().toString()));
+                adapter.notifyDataSetChanged();
+                Toast.makeText(VideoCallActivity.this, "Image Received, TAP to check", Toast.LENGTH_SHORT).show();
+            }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                arrayList.add(new DataUploads(snapshot.getKey().toString(), snapshot.getValue().toString()));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                arrayList.remove(new DataUploads(snapshot.getKey().toString(), snapshot.getValue().toString()));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
+
+        private Context context;
+        private ArrayList<DataUploads> list=new ArrayList<>();
+
+        public ImageAdapter(Context context, ArrayList<DataUploads> list) {
+            this.context=context;
+            this.list=list;
         }
 
+        @NonNull
+        @Override
+        public ImageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_call, parent, false);
+            return new ViewHolder(view);
+        }
+        @Override
+        public void onBindViewHolder(@NonNull ImageAdapter.ViewHolder holder, final int position) {
+
+            Picasso.get().load(list.get(position).getUrl()).into(holder.imageView);
+        }
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView tvName, tvDesc;
+            ImageView imageView;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView=itemView.findViewById(R.id.list_call_imageview);
+            }
+        }
     }
 
     @Override
@@ -120,8 +187,8 @@ public class VideoCallActivity extends BaseActivity implements  Session.SessionL
         String[] perms = { Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO };
         if (EasyPermissions.hasPermissions(this, perms)) {
 
-            mPublisherViewContainer = (FrameLayout)findViewById(R.id.publisher_container);
-            mSubscriberViewContainer = (FrameLayout)findViewById(R.id.subscriber_container);
+            mPublisherViewContainer = findViewById(R.id.publisher_container);
+            mSubscriberViewContainer = findViewById(R.id.subscriber_container);
 
 
             mSession = new Session.Builder(this, API_KEY, SESSION_ID).build();
@@ -151,11 +218,10 @@ public class VideoCallActivity extends BaseActivity implements  Session.SessionL
         mSession.publish(mPublisher);
     }
 
-
     @Override
     public void onDisconnected(Session session) {
         Log.i(LOG_TAG, "Session Disconnected");
-        ref2.setValue("");
+        ref_session.setValue("");
     }
 
     @Override
@@ -212,9 +278,26 @@ public class VideoCallActivity extends BaseActivity implements  Session.SessionL
     @Override
     public boolean onGesture(GlassGestureDetector.Gesture gesture) {
 
-        if (gesture == GlassGestureDetector.Gesture.SWIPE_DOWN){
-            mSession.disconnect();
+        switch (gesture){
+            case SWIPE_DOWN:
+                if (rcView.getVisibility() == View.VISIBLE){
+                    rcView.setVisibility(View.GONE);
+                }else {
+                    mSession.disconnect();
+                    ref_session.setValue("");
+                }
+                break;
+
+            case TAP:
+                if (rcView.getVisibility() == View.VISIBLE){
+                    rcView.setVisibility(View.GONE);
+                }else {
+                    rcView.setVisibility(View.VISIBLE);
+                }
+                break;
         }
-        return super.onGesture(gesture);
+        return true;
     }
+
+
 }
